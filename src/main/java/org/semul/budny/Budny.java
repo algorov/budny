8fb8cafa7f483feea13b5d25eebc7eec548c72e8
SEMUL <semul.main@icloud.com>
2023-08-11 19:20:28 +0300

    added keyboard input

--- src/main/java/org/semul/budny/Budny.java
+++ src/main/java/org/semul/budny/Budny.java
@@ -4,6 +4,10 @@ package org.semul.budny;
 import org.semul.budny.exception.ExeptionCount;
 import org.semul.budny.manager.Manager;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Budny {
     public static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(Budny.class);
     private final Manager manager;
@@ -17,6 +21,19 @@ public class Budny {
         logger.info("Initialization.");
 
         Budny app = new Budny();
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//
+//        try {
+//            logger.info("Input data.");
+//            System.out.print("Enter the login >>> ");
+//            String login = reader.readLine();
+//            System.out.print("Enter the password >>> ");
+//            String password = reader.readLine();
+//            app.signIn(login, password);
+//        } catch (IOException e) {
+//            logger.error(e);
+//            throw new RuntimeException(e);
+//        }
         app.signIn(args[0], args[1]);
 
         while (app.manager.getActiveAccountsCount() > 0 && ExeptionCount.count <= 5) {
