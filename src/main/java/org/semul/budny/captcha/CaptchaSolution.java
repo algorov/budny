16440d1ff67f05f0a1a66cb7ae9aeaeb93763e31
SEMUL <semul.main@icloud.com>
2023-08-18 04:43:08 +0300

    upd: secrets

--- src/main/java/org/semul/budny/captcha/CaptchaSolution.java
+++ src/main/java/org/semul/budny/captcha/CaptchaSolution.java
@@ -3,9 +3,34 @@ package org.semul.budny.captcha;
 import com.twocaptcha.TwoCaptcha;
 import com.twocaptcha.captcha.Normal;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CaptchaSolution {
     public static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(CaptchaSolution.class);
-    public static String API_KEY = "df1293f1a441c150294d5838fd0b1cb7";
+    public static String API_KEY = setApiKey();
+
+    public static String setApiKey() {
+        StringBuilder builder = new StringBuilder();
+
+        try (FileReader reader = new FileReader(System.getProperty("user.dir") + "/config/secret.txt")) {
+            int symb;
+            int cursor = 0;
+            while ((symb = reader.read()) != -1) {
+                if (cursor > 7) {
+                    builder.append((char) symb);
+                }
+
+                cursor++;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return builder.toString();
+    }
 
     public static String solution(String captchaPath) {
         logger.info("Captcha solution");
